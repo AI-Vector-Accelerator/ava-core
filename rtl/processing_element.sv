20c9eb53df4f85eae53e91fc2273c4e7aca451c3
Byron Theobald <byrontheobald@gmail.com>
2020-11-02 15:39:06 +0000

    Added signed to PE internal logic

--- rtl/processing_element.sv
+++ rtl/processing_element.sv
@@ -1,10 +1,10 @@
 module processing_element 
-    (input logic [7:0] a,
-     input logic [7:0] b,
-     input logic [7:0] c,
+    (input logic signed [7:0] a,
+     input logic signed [7:0] b,
+     input logic signed [7:0] c,
      
-     input logic [11:0] tree_l,
-     input logic [11:0] tree_r,
+     input logic signed [11:0] tree_l,
+     input logic signed [11:0] tree_r,
      
      input logic [1:0] mux_add_a,
      input logic [1:0] mux_add_b,
@@ -17,23 +17,23 @@ module processing_element
      input logic clk,
      input logic n_reset,
 
-     output logic [7:0] r,
-     output logic [11:0] tree_o);
+     output logic signed [7:0] r,
+     output logic signed [11:0] tree_o);
 
-    logic [15:0] mul_out;
-    logic [11:0] mul_out_trunc;
+    logic signed [15:0] mul_out;
+    logic signed [11:0] mul_out_trunc;
     assign mul_out = a * b;
     assign mul_out_trunc = mul_out[15:4];
 
-    logic [11:0] a_ex, b_ex, c_ex;
+    logic signed [11:0] a_ex, b_ex, c_ex;
     bit_ext #(.W_IN(8), .W_OUT(12)) a_bex(a, a_ex);
     bit_ext #(.W_IN(8), .W_OUT(12)) b_bex(b, b_ex);
     bit_ext #(.W_IN(8), .W_OUT(12)) c_bex(c, c_ex);
     
-    logic [11:0] acc_reg, c_acc;
-    assign c_acc = mux_c_acc ? c_ex : acc_reg;
+    logic signed [11:0] acc_reg, c_acc;
+    assign c_acc = mux_c_acc ? acc_reg : c_ex;
 
-    logic [11:0] add_a, add_b;
+    logic signed [11:0] add_a, add_b;
     always_comb begin
         case(mux_add_a)
             2'd0: add_a = a_ex;
@@ -51,11 +51,11 @@ module processing_element
         endcase
     end
 
-    logic [12:0] add_out;
+    logic signed [12:0] add_out;
     assign add_out = add_a + add_b;
 
-    logic [12:0] sat8_in;
-    logic [7:0] sat8_out;
+    logic signed [12:0] sat8_in;
+    logic signed [7:0] sat8_out;
 
     always_comb begin
         case(mux_sat8)
@@ -69,21 +69,20 @@ module processing_element
     end
     sat_unit #(.W_IN(13), .W_OUT(8)) sat_unit_8 (sat8_in, sat8_out);
 
-
-    logic [11:0] add_sat12_out;
+    logic signed [11:0] add_sat12_out;
     sat_unit #(.W_IN(13), .W_OUT(12)) sat_unit_12 (add_out, add_sat12_out);
     assign tree_o = add_sat12_out;
 
-    logic [7:0] relu_in;
-    logic [6:0] relu_out; // Unsigned
+    logic signed [7:0] relu_in;
+    logic unsigned [6:0] relu_out;
     assign relu_in = mux_relu ? sat8_out : a;
     relu_bound #(.W(8)) relu6 (relu_in, relu_out);
  
-    logic [11:0] sat8_out_ex, relu_out_ex;
+    logic signed [11:0] sat8_out_ex, relu_out_ex;
     bit_ext #(.W_IN(8), .W_OUT(12)) sat8_bex(sat8_out, sat8_out_ex);
     assign relu_out_ex = {5'b0, relu_out};
 
-    logic [11:0] res_out;
+    logic signed [11:0] res_out;
 
     always_comb begin
         case(mux_res)
@@ -104,7 +103,7 @@ module processing_element
         end
     end
 
-    logic [11:0] wb_sat8_in;
+    logic signed [11:0] wb_sat8_in;
     assign wb_sat8_in = mux_comb ? res_out : acc_reg;
 
     //sat_unit #(.W_IN(12), .W_OUT(8)) wb_sat8 (wb_sat8_in, r);
